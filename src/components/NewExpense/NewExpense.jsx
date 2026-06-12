ce931f8fd08f27a8a7dc4e27a286f42d58f18a68
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-06-12 17:00:44 +0530

    Merge pull request #1 from rajdeepX/migrate-to-tailwind
    Migrate to tailwind

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,7 +1,4 @@
-import React from "react";
 import ExpenseForm from "./ExpenseForm";
-
-import "./NewExpense.css";
 
 const NewExpense = ({ onAddExpenseData }) => {
   const inputExpensesHandler = (expenseItems) => {
@@ -9,7 +6,7 @@ const NewExpense = ({ onAddExpenseData }) => {
   };
 
   return (
-    <div className="new-expense">
+    <div className="bg-[#780000] p-4 my-8 mx-auto w-200 max-w-[95%] rounded-xl text-center shadow-[0_1px_8px_rgba(0,0,0,0.25)] [&_button]:font-inherit [&_button]:cursor-pointer [&_button]:py-4 [&_button]:px-8 [&_button]:border [&_button]:border-[#fdf0d5] [&_button]:bg-[#fdf0d5] [&_button]:text-[#003049] [&_button]:rounded-xl [&_button]:mr-4 [&_button:hover]:bg-[#f0c673] [&_button:hover]:border-[#f0c673] [&_button:active]:bg-[#f0c673] [&_button:active]:border-[#f0c673] [&_button.alternative]:text-[#220131] [&_button.alternative]:border-transparent [&_button.alternative]:bg-transparent [&_button.alternative:hover]:bg-[#ddb3f8] [&_button.alternative:active]:bg-[#ddb3f8]">
       <ExpenseForm onInputExpenses={inputExpensesHandler} />
     </div>
   );
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,6 @@
-import "./Card.css";
-
 const Card = (props) => {
-  const classes = "card " + props.className;
+  const classes =
+    "rounded-xl shadow-[0_1px_8px_rgba(0,0,0,0.25)] " + (props.className || "");
 
   return <div className={classes}>{props.children}</div>;
 };
