fcacfa98dcb66101b227e3b4cd6cb3d1b24a49fa
Rajdeep Dutta <109196532+rajdeepX@users.noreply.github.com>
2026-06-12 13:18:39 +0530

    update ExpenseDate.jsx

--- src/components/Expenses/ExpenseDate.jsx
+++ src/components/Expenses/ExpenseDate.jsx
@@ -1,15 +1,13 @@
-import "./ExpenseDate.css";
-
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
   return (
-    <div className="expense-date">
-      <div className="expense-date__month">{month}</div>
-      <div className="expense-date__year">{year}</div>
-      <div className="expense-date__day">{day}</div>
+    <div className="flex flex-col w-[88px] h-[88px] border border-[#ececec] bg-[#2a2a2a] text-white rounded-xl items-center justify-center">
+      <div className="text-xs font-bold">{month}</div>
+      <div className="text-xs">{year}</div>
+      <div className="text-2xl font-bold">{day}</div>
     </div>
   );
 };
